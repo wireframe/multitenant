2f641600cbc66408a2ed3e46118da0a434fb9d5b
Omer Doron <omer@dapulse.com>
2017-12-07 08:15:25 +0200

    Replace return with next

--- lib/multitenant.rb
+++ lib/multitenant.rb
@@ -24,7 +24,7 @@ module Multitenant
     def belongs_to_multitenant(association = :tenant)
       reflection = reflect_on_association association
       before_validation Proc.new {|m|
-        return unless Multitenant.current_tenant
+        next unless Multitenant.current_tenant
         tenant = m.send "#{association}"
         if tenant.nil? then
           m.send "#{association}=".to_sym, Multitenant.current_tenant
@@ -35,7 +35,7 @@ module Multitenant
       
       # Prevent updating objects to a different tenant
       before_save Proc.new {|m|
-        return unless Multitenant.current_tenant
+        next unless Multitenant.current_tenant
         tenant = m.send "#{association}".to_sym
         raise AccessException, "Trying to update object in to tenant #{tenant.id} while in current_tenant #{Multitenant.current_tenant.id}" unless tenant.id == Multitenant.current_tenant.id
       }
